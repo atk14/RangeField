e1d626603d9865a9ae2f48bf081e1eb86ef7b211
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-08-27 13:50:11 +0200

    Initial commit

---
+++ src/app/fields/range_field.php
@@ -0,0 +1,118 @@
+<?php
+class RangeField extends Field {
+	function __construct($options)  {
+		$options+= [
+			'min_value' => null,
+			'max_value' => null,
+			'widget_range' => null,
+			'autocorrect' => false,
+			'unbounded' => false,
+			'error_messages' => [],
+			'widget_options' => []
+		];
+
+		$this->autocorrect = $options['autocorrect'];
+		$options['error_messages'] += [
+			'required_min' => _('Minimum of the range is required'),
+			'required_max' => _('Maximum of the range is required'),
+			'out_of_range' => _('Value is out of range of allowed values'),
+			'invalid' => _('Invalid input for range field'),
+		];
+
+		$this->range = [
+			'min' => $options['min_value'],
+			'max' => $options['max_value']
+		];
+		$wrange = $options['widget_range']?$wrange:$this->range;
+
+		$options+= [
+			'widget' => new RangeInput($options['widget_options'] + [
+				'min_value' => $wrange['min'],
+				'max_value' => $wrange['max'],
+				'unbounded' => $options['unbounded'],
+			])
+		];
+
+		$this->unbounded = $options['unbounded'];
+
+		parent::__construct($options);
+	}
+
+	function set_range($range, $for = ['widget', 'field']) {
+		if(!is_array($for)) {
+			$for = [ $for ];
+		}
+		if(in_array('field', $for)) {
+			$this->range = $range;
+		}
+		if(in_array('widget', $for)) {
+			$this->widget->range = $range;
+		}
+	}
+
+	function checkRange(&$v, $role) {
+		if($v === null || $v === '') {
+			if($this->autocorrect) {
+				$v = $this->unbounded?null : $this->range[$role];
+			}
+			if($this->required) {
+				return $this->messages['required'];
+			}
+			return false;
+		}
+
+		if($this->range['min'] !== null && ($v < $this->range['min'])) {
+			if($this->autocorrect) {
+				$v = $this->range['min'];
+			} else {
+				if($v===null) {
+					return $this->messages['required_'.$role] ;
+				} else {
+					return $this->messages['out_of_range'];
+				}
+			}
+		}
+
+		if($this->range['max'] !== null && $v > $this->range['max']) {
+				if($this->autocorrect) {
+					$v = $this->range['max'];
+				} else {
+					if($v===null) {
+						return $this->messages['required_'.$role] ;
+					} else {
+						return $this->messages['out_of_range'];
+					}
+				}
+		}
+
+		if( $this->unbounded && $v == $this->range[$role] ) {
+			$v = null;
+		}
+		return false;
+	}
+
+	function clean($value) {
+		if(!is_array($value)) {
+			if($this->autocorrect) {
+				return [null, $this->unbounded ? [] : $this->range ];
+			}
+			return [$this->messages['invalid'],null];
+		}
+		$value = array_intersect_key($value, ['min'=>1, 'max' =>1]) +
+			['min' => null, 'max' => null];
+		$value = array_map( function($v) { return $v==''? null:$v; }, $value);
+
+		if( $value['min'] !== null && $value['max'] !== null && $value['min'] > $value['max'] ) {
+			$value['min']=$value['max'];
+		}
+
+		foreach($value as $k => &$v) {
+			if($e = $this->checkRange($v, $k)) {
+				return [ $e, null ];
+			}
+		}
+		unset($v);
+		return [null, $value];
+	}
+
+}
---
+++ src/app/widgets/range_input.php
@@ -0,0 +1,108 @@
+<?php
+/**
+ */
+class RangeInput extends Widget
+{
+	var $input_type = 'number';
+
+	/**
+	 * Constructor
+	 *
+	 * @param array $options
+	 */
+	function __construct($options = array()){
+		$options += [
+			'min_value' => null,
+			'max_value' => null,
+			'params' => [],
+			'step' => 1,
+			'format' => null,			//see javascript NoUISlider.formats
+			'format_arguments' => null,
+			'unbounded' => false   //returns null for the border values
+		];
+		$this->range= [
+			'min' => $options['min_value'] ?:0,
+			'max' => $options['max_value'] ?:1
+		];
+		parent::__construct($options);
+		$this->params = $options['params'];
+		$this->unbounded = $options['unbounded'];
+		$this->step = $options['step'];
+		$this->format = $options['format'];
+		$this->format_arguments = $options['format_arguments'];
+	}
+
+	function set_range($range) {
+		$this->range = $range;
+	}
+
+	function value($value, $role) {
+		if( $value[$role] && $value[$role] != $this->range[$role] ) {
+			return $value[$role];
+		}
+		return $this->unbounded ? null : $this->range[$role];
+	}
+
+	function render($name, $value, $options=array()) {
+		$options = forms_array_merge(array('attrs'=> null), $options);
+		$out =
+			_('od') .
+			$this->input($name, $value, 'min', $options) .
+			_('do') .
+			$this->input($name, $value, 'max', $options);
+
+		$data = $this->params;
+		if( $value['min'] === null ) {
+			$value['min'] = $this->range['min'];
+		}
+		if( $value['max'] === null ) {
+			$value['max'] = $this->range['max'];
+		}
+
+		$data['start'] = [ $value['min'], $value['max'] ];
+		$data['range'] = $this->range;
+		$data['step'] = $this->step;
+		$data['unbounded'] = $this->unbounded;
+		if($this->format) {
+			$data['format'] = $this->format;
+		}
+		if($this->format_arguments) {
+			$data['format_arguments'] = $this->format_arguments;
+		}
+
+		$data = json_encode($data);
+
+		$class_disabled=$this->disabled?' noui-slider--disabled':'';
+
+		return "
+			<div class='noui-slider__wrapper'>
+				<div class='noui-slider$class_disabled' data-noui-slider='$data'>
+					<div class='noui-slider-hide'>
+					$out
+					</div>
+				</div>
+			</div>
+		";
+	}
+
+	function input($name, $value, $role, $options) {
+
+		$final_attrs = $this->build_attrs([
+			'type' => $this->input_type,
+			'value' => $this->value($value, $role),
+			'name' => "{$name}[$role]",
+			'class' => "noui-slider-$role" ],
+			$options['attrs']
+		);
+		$final_attrs["id"] = $final_attrs["id"].($role == 'min'?'':'_max'); // "id_name" or "id_name_max"
+
+		if($this->range['min']!==null) {
+			$final_attrs['min'] = $this->range['min'];
+		}
+		if($this->range['max']!==null) {
+			$final_attrs['max'] = $this->range['max'];
+		}
+
+		return '<input'.flatatt($final_attrs).' />';
+	}
+}
